8cbcf645a38eabc80a15767a258c5b5b175eabe7
jianhuap <jianhuap@vmware.com>
2017-06-26 14:49:28 +0800

    Update nsx_network_input.py

--- nsx_network_input.py
+++ nsx_network_input.py
@@ -4,4 +4,33 @@
 #
 
 # getNetwork test case configuration
-NSX_NETWORK_GET_ID = 'virtualwire-1'
+NSX_NETWORK_GET_ID = 'virtualwire-5'
+
+
+# createNetwork test case configuration
+NSX_NETWORK_CREATE_SCOPE_ID = 'vdnscope-1'
+NSX_NETWORK_CREATE_REQ_BODY = '''
+<virtualWireCreateSpec>
+    <name>Web-Tier-01</name>
+    <description>Web tier network</description>
+    <tenantId>WPtenant</tenantId>
+    <controlPlaneMode>UNICAST_MODE</controlPlaneMode>
+    <guestVlanAllowed>false</guestVlanAllowed>
+</virtualWireCreateSpec>
+'''
+
+
+# updateNetwork test case configuration
+NSX_NETWORK_UPDATE_ID = 'virtualwire-5'
+NSX_NETWORK_UPDATE_REQ_BODY = '''
+<virtualWire>
+    <name>ULS-Web-Tier-02 </name>
+    <description>Universal Web Logical Switch</description>
+    <tenantId>virtual wire tenant</tenantId>
+    <controlPlaneMode>UNICAST_MODE</controlPlaneMode>
+</virtualWire>
+'''
+
+
+# deleteNetwork test case configuration
+NSX_NETWORK_DELETE_ID = 'virtualwire-5'
