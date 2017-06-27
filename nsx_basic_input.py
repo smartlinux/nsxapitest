3b9f12a20ba9aa86aec511cacc38e0a6da15ca8e
jianhuap <jianhuap@vmware.com>
2017-06-27 14:41:10 +0800

    Update nsx_basic_input.py

--- nsx_basic_input.py
+++ nsx_basic_input.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 #
 # Basic configration of NSX & vSphere environment.
@@ -8,3 +7,7 @@ NSX_IP = '172.16.10.36'
 NSX_USER = 'admin'
 NSX_PWD = 'VMware1!'
 NSX_URL = 'https://' + NSX_IP
+
+VC_IP = '172.16.10.18'
+VC_USER = 'administrator@vsphere.local'
+VC_PWD = 'VMware1!'
