829c4d8c7f8978d790223c4ddeaf5641d1ffc76e
jianhuap <jianhuap@vmware.com>
2017-07-04 14:46:29 +0800

    Create case04_nsx_network_get_input.py

---
+++ network/case04_nsx_network_get_input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Network function test configration of NSX & vSphere environment.
+#
+
+# 要获取网络的ID，该ID可以在network_list的执行结果中找到，或者在web client->logical switch->Summary界面找到
+NSX_NETWORK_GET_ID = 'virtualwire-1'
