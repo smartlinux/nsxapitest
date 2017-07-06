31343c2b7ac3d32461eb4effc0d8db3eed791f09
smartlinux <anzhiping@gmail.com>
2017-07-06 03:59:00 -0700

    comments

--- securitygroup/case60_nsx_securitygroup_update_input.py
+++ securitygroup/case60_nsx_securitygroup_update_input.py
@@ -9,7 +9,7 @@ NSX_SECURITYGROUP_UDPATE_ID = 'securitygroup-10'
 # updateSecurityGroup test case configuration
 
 
-# revison后面的数字可能需要根据实际情况修改
+# revison后面的数字可能需要根据实际情况修改，只能大于等于原来的版本号
 
 NSX_SECURITYGROUP_UPDATE_REQ_BODY = '''
     <securitygroup>
