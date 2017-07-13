05f0ffce7dcf832430982a68a18de82c3a963ed7
Zhiping <zan@vmware.com>
2017-07-13 15:47:28 +0800

    Update case63_nsx_securitygrouprule_get_input.py

--- securitygroup/case63_nsx_securitygrouprule_get_input.py
+++ securitygroup/case63_nsx_securitygrouprule_get_input.py
@@ -4,6 +4,8 @@
 # securitygroup function test configration of NSX & vSphere environment.
 #
 
-
+#指定securitygroup ID
 NSX_SECURITYGROUP_GET_ID ='policy-5'
+
+#指定需要获取的rule ID
 NSX_RULE_GET_ID ='firewallpolicyaction-5'
