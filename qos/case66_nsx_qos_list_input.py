2fb18e94fca840e0cacb7a98de8df79009210410
jianhuap <jianhuap@vmware.com>
2017-07-25 18:15:51 +0800

    add comments to qos scripts.

--- qos/case66_nsx_qos_list_input.py
+++ qos/case66_nsx_qos_list_input.py
@@ -4,5 +4,5 @@
 # QOS function test configration of NSX & vSphere environment.
 #
 
-# QOS所在的Edge ID列表，可以在web client-> NSX edges里找到
+# QOS所在的Edge ID列表（不能是DLR），可以在web client-> NSX edges里找到
 NSX_EDGE_ID_LIST = ['edge-2', 'edge-9']
--- qos/case67_nsx_qos_get_input.py
+++ qos/case67_nsx_qos_get_input.py
@@ -4,7 +4,7 @@
 # QOS function test configration of NSX & vSphere environment.
 #
 
-# QOS所在的Edge ID，可以在web client-> NSX edges里找到
+# QOS所在的Edge ID（不能是DLR），可以在web client-> NSX edges里找到
 NSX_EDGE_ID = 'edge-9'
 
 # Edge Interface Index，可以在web client-> NSX edges->Manage->Settings->Interface->vNIC#里找到
