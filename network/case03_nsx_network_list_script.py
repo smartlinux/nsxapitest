5e005ffb80b6f9bffc57b214aa8888c81dd33b0a
jianhuap <jianhuap@vmware.com>
2017-07-25 17:11:59 +0800

    modify network scripts.

--- network/case03_nsx_network_list_script.py
+++ network/case03_nsx_network_list_script.py
@@ -3,19 +3,24 @@
 #
 # Test script for network function.
 # 
-# Network 映射为 NSX Logincal Switch, 下面是属性映射关系:
-#   ID(objectId)、名称(name)、管理状态()、状态()、是否共享(isUniversal)、租户ID(tenantId)
+# Network 映射为 NSX Logincal Switch(virtualWire), 下面是属性映射关系:
+#   ID(objectId)、名称(name)、管理状态(mgmtStatus)、状态(status)、是否共享(isUniversal)、租户ID(tenantId)
 #   网络类型(backingType)、物理网络(vdsContextWithBacking)、vlanID(vlanId/vdnId)、qos规则ID(inShapingPolicy/outShapingPolicy/qosTag)
 #
 
 import sys
 import datetime
 import libxml2
+import ssl
+import atexit
 
 sys.path.append("..")
 import rest
 
 from nsx_basic_input import *
+
+from pyVim.connect import SmartConnect, Disconnect
+from pyVmomi import vim
 
 caseName = 'case03_nsx_network_list'
 restclient = rest.Rest(NSX_IP, NSX_USER, NSX_PWD, True)
@@ -23,7 +28,38 @@ restclient = rest.Rest(NSX_IP, NSX_USER, NSX_PWD, True)
 
 def listNetworks():
     respData = restclient.get(NSX_URL+'/api/2.0/vdn/virtualwires', 'listNetworks')
+    dpgMap = getPortGroupMap()
+    respDoc = libxml2.parseDoc(respData)
+    xp = respDoc.xpathNewContext()
+    nodes = xp.xpathEval("//virtualWires/dataPage/virtualWire")
+
+    for ls in nodes:
+        xp.setContextNode(ls)
+        dpgId = xp.xpathEval("vdsContextWithBacking/backingValue")[0].content
+        dpg = dpgMap[dpgId]
+        if dpg == None:
+            continue;
+        ls.newChild(None, 'status', dpg.overallStatus)
+        ls.newChild(None, 'mgmtStatus', dpg.configStatus)
+    
+    respData = respDoc.serialize('UTF-8', 1)
     output(restclient.getDebugInfo() + restclient.prettyPrint(respData))
+
+
+def getPortGroupMap():
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    context.verify_mode = ssl.CERT_NONE
+    serviceInstance = SmartConnect(host=VC_IP, user=VC_USER, pwd=VC_PWD, port=443, sslContext=context)
+    atexit.register(Disconnect, serviceInstance)
+    content = serviceInstance.RetrieveContent()
+
+    dpg_view = content.viewManager.CreateContainerView(content.rootFolder,[vim.dvs.DistributedVirtualPortgroup],True)
+    dpgs = [dpg for dpg in dpg_view.view]
+    dpg_view.Destroy()
+    dpgMap = {}
+    for dpg in dpgs:
+        dpgMap[dpg.key] = dpg
+    return dpgMap
 
 
 def output(msg):
--- network/case04_nsx_network_get_script.py
+++ network/case04_nsx_network_get_script.py
@@ -4,7 +4,7 @@
 # Test script for network function.
 # 
 # Network 映射为 NSX Logincal Switch, 下面是属性映射关系:
-#   ID(objectId)、名称(name)、管理状态()、状态()、是否共享(isUniversal)、租户ID(tenantId)
+#   ID(objectId)、名称(name)、管理状态(mgmtStatus)、状态(status)、是否共享(isUniversal)、租户ID(tenantId)
 #   网络类型(backingType)、物理网络(vdsContextWithBacking)、vlanID(vlanId/vdnId)、qos规则ID(inShapingPolicy/outShapingPolicy/qosTag)
 #
 
@@ -29,19 +29,29 @@ restclient = rest.Rest(NSX_IP, NSX_USER, NSX_PWD, True)
 
 def getNetwork():
     respData = restclient.get(NSX_URL+'/api/2.0/vdn/virtualwires/'+NSX_NETWORK_ID, 'getNetwork')
-    outputstr = restclient.getDebugInfo() + restclient.prettyPrint(respData)
+    outputstr = restclient.getDebugInfo()
     
-    outputstr += "\nBacking network info by vSphere API:\n"
     respDoc = libxml2.parseDoc(respData)
     xp = respDoc.xpathNewContext()
-    nodes = xp.xpathEval("//virtualWire/vdsContextWithBacking/backingValue")
-    for n in nodes:
-        portgroupid = n.content.strip()
-        outputstr += getPortGroupInfo(portgroupid)
+    dpgId = xp.xpathEval("//virtualWire/vdsContextWithBacking/backingValue")[0].content
+    dpg = getPortGroupMap()[dpgId]
+
+    if dpg != None:
+        ls = xp.xpathEval("//virtualWire")[0]
+        ls.newChild(None, 'status', dpg.overallStatus)
+        ls.newChild(None, 'mgmtStatus', dpg.configStatus)
+        respData = respDoc.serialize('UTF-8', 1)
+
+    outputstr += restclient.prettyPrint(respData)
+
+    # if dpg != None:
+    #     outputstr += "\nBacking network info by vSphere API:\n"
+    #     outputstr += str(dpg.config.defaultPortConfig)
+
     output(outputstr)
 
 
-def getPortGroupInfo(portgroupid):
+def getPortGroupMap():
     context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
     context.verify_mode = ssl.CERT_NONE
     serviceInstance = SmartConnect(host=VC_IP, user=VC_USER, pwd=VC_PWD, port=443, sslContext=context)
@@ -51,13 +61,10 @@ def getPortGroupInfo(portgroupid):
     dpg_view = content.viewManager.CreateContainerView(content.rootFolder,[vim.dvs.DistributedVirtualPortgroup],True)
     dpgs = [dpg for dpg in dpg_view.view]
     dpg_view.Destroy()
-
+    dpgMap = {}
     for dpg in dpgs:
-        # print("Found DPG:", dpg.key)
-        if dpg.key == portgroupid:
-            return str(dpg.config.defaultPortConfig)
-
-    return 'Cannot find portgroup, id: ' + portgroupid
+        dpgMap[dpg.key] = dpg
+    return dpgMap
 
 
 def output(msg):
